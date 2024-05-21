f4bbc611e75b0dfd1d04e776344a07d32aa74934
Imam Septian <imamseptian.wijaya@gmail.com>
2024-05-21 22:38:54 +0700

    feat: add component to wrap framer motion component

---
+++ src/components/framer/motion-div.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+const MotionDiv = motion.div;
+export default MotionDiv;
---
+++ src/components/framer/motion-layout-group.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { LayoutGroup } from 'framer-motion';
+
+const MotionLayoutGroup = LayoutGroup;
+export default MotionLayoutGroup;
